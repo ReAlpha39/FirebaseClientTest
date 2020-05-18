af172fac07bc0d7353dcf02b564e16a21b299fbc
YunusNS <yunusw718@gmail.com>
2020-05-18 13:03:50 +0800

    Initial commit

---
+++ app/src/main/java/id/nyaa/testbarang/MainActivity.java
@@ -0,0 +1,35 @@
+package id.nyaa.testbarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bTambah;
+    private Button bLihat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bTambah = findViewById(R.id.btnTambah);
+        bLihat = findViewById(R.id.btnLihat);
+
+        bTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TambahDta.getActIntent(MainActivity.this));
+            }
+        });
+        bLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
